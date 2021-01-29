eaf9b2d1c159aeee4c65442f4fa3128c0bdbea68
Joe Laurole <Josue.laurole@gmail.com>
2021-01-29 11:47:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const second hand = document.querySelector('.second-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = ((econds / 60) * 360);
+  
+  console.log(seconds);
+  }
+  
+  SetInterval(setDate, 1000);
+  
+  </script>
+  </body>
+  </html>
